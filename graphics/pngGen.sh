8ff32d2dfccef4e271a2033131a82aeba427385f
Chen Yu <chenyu.nus@gmail.com>
2016-06-05 17:31:53 +0800

    Changed logo to svg, updated favicon

--- graphics/pngGen.sh
+++ graphics/pngGen.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Requires Inkscape
-inkscape -z -f square.svg -w 558 -j -e ../tile.png
-inkscape -z -f square.svg -w 180 -j -e ../apple-touch-icon.png
-convert -resize 32x32 "square.svg" -colors 256 "../favicon.ico"
-cp Basic.svg ../images/logo.svg
+inkscape -z -f square2.svg -w 558 -j -e ../tile.png
+inkscape -z -f square2.svg -w 180 -j -e ../apple-touch-icon.png
+inkscape -z -f square2.svg -w 32 -j -e temp.png
+convert -resize 32x32 "temp.png" -colors 256 "../favicon.ico"
+rm temp.png
+# convert -resize 32x32 "square2.svg" -colors 256 "../favicon.ico"
+# cp Basic.svg ../images/logo.svg
